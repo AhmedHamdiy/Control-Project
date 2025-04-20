dbebf5c59244b70b7dc49727971d7d07a923a46c
ahmedHamdiy <ah2882003@gmail.com>
2025-04-20 21:14:59 +0200

    finish req 1,2,3

--- code.m
+++ code.m
@@ -1,21 +1,26 @@
-% Main script body
-A1 = 5; A2 = 4; R1 = 3; R2 = 5;  % Define parameters first
+A1 = 5; A2 = 4; R1 = 3; R2 = 5;
 % State-space matrices
+
+% Assume our state is [h1, h2] , input is Qin, outputs are Q2, Q1, H1, H2
+% State Matrix
+
 A = [-1/(A1*R1), 1/(A1*R1);        % dh1/dt equation
       1/(A2*R1), -(1/(A2*R1) + 1/(A2*R2))];  % dh2/dt equation
 
+% Input Matrix
 B = [1/A1; 0];                     % Input only affects dh1/dt
 
+% Output Matrix
 C = [0, 1/R2;                      % q_out output
      1/R1, -1/R1;                  % q1 output
      1, 0;                         % h1 output
      0, 1];                        % h2 output
 
-D = zeros(4,1);                    % No direct feedthrough
-% Define state-space system
+% Direct Feedthrough Matrix
+D = zeros(4,1);            
+
 sys = ss(A,B,C,D,'InputName','Qin','OutputName',{'Q2','Q1','H1','H2'});
 
-% Create arrays of transfer functions and their names
 transfer_functions = {
     tf(sys(4)),  % H2/Qin
     tf(sys(3)),  % H1/Qin
@@ -25,12 +30,10 @@ transfer_functions = {
 
 names = {'H2(S)/Qin(S)', 'H1(S)/Qin(S)', 'Q1(S)/Qin(S)', 'Q2(S)/Qin(S)'};
 
-% Loop through each transfer function
 for k = 1:length(transfer_functions)
     tf_current = transfer_functions{k};
     [num, den] = tfdata(tf_current, 'v');
     
-    % Print transfer function name
     fprintf('\n\n\n%s = \n\n', names{k});
     
     % Print numerator
@@ -76,7 +79,6 @@ fprintf('\n\n\nPoles of H2/Qin: P0 = %.4f, P1 = %.4f\n', P(1), P(2));
 is_stable = isstable(tf_H2_Qin);
 fprintf('Is stable: %d\n', is_stable);
 
-% Plot pole-zero map
 figure;
 pzmap(tf_H2_Qin);
 title('Pole-Zero Map of H2/Qin');
